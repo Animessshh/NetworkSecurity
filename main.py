ceb7c5a6e69f424754e4ac82dd1183fb3f42d363
Animesh Sharma <animesh1205sharma@gmail.com>
2026-09-17 16:35:21 +0530

    Data validation Component implemented

--- main.py
+++ main.py
@@ -1,53 +1,29 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
-if __name__ == "__main__":
-    try:
 
-        print("1. Starting pipeline")
-
-        trainingpipelineconfig = TrainingPipelineConfig()
-
-        print("2. TrainingPipelineConfig created")
-
-        dataingestionconfig = DataIngestionConfig(
-            training_pipeline_config=trainingpipelineconfig
-        )
-
-        print("3. DataIngestionConfig created")
-
-        data_ingestion = DataIngestion(
-            data_ingestion_config=dataingestionconfig
-        )
-
-        print("4. DataIngestion object created")
-
-        logging.info("Initiate the data ingestion")
-
-        print("5. Starting data ingestion")
-
-        dataingestionartifact = data_ingestion.initiate_data_ingestion()
-
-        print("6. Data ingestion completed")
-
-        print(dataingestionartifact)
-
-    except Exception as e:
-        raise NetworkSecurityException(e, sys)
-
-
-'''if __name__=="__main__":
+if __name__=="__main__":
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(training_pipeline_config=trainingpipelineconfig)
+        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(data_ingestion_config=dataingestionconfig)
         logging.info("Initiate the data ingestion")
         
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation completed")
         print(dataingestionartifact)
+        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
+        logging.info("Initiate the data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+        
+        
     except Exception as e:
-        raise NetworkSecurityException(e,sys)'''
+        raise NetworkSecurityException(e,sys)
